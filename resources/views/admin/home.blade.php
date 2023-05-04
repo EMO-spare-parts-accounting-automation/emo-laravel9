0cbf59a4e47264732ac9090963d3bc30f65422f0
enesbayri <117692313+enesbayri@users.noreply.github.com>
2023-05-05 02:51:25 +0300

    Authentication işlemleri yapıldı.login ve register arkaplanları ayarlandı.customer ve admine özel sayfa oluşturuldu.Emo database oluşturuldu.users tablo ve user model düzenlendi

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,27 @@
+
+
+<!--burdan itibaren admin sayfa düzenlemeleri admin klasöründe olmak şartıyla yapılabilir-->
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('ADMİN PAGE') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <center><h1>HOŞGELDİN {{$user->name}}!</h1></center> <!--ismi ile karşılama mesajı-->
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
