323998ea041b32c8653a63f79c8c1f935faa85fd
enesbayri <117692313+enesbayri@users.noreply.github.com>
2023-05-13 16:55:25 +0300

    Guvenlik guncellemesi yapildi.login register headerlari guncellendi.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('EMO PARÇA DÜNYASI MÜŞTERİSİ OLUN!') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -64,7 +64,7 @@
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
+                                    {{ __('Müşteri Ol!') }}
                                 </button>
                             </div>
                         </div>
