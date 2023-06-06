a5d13d87d0f0d70b7af689ebb4f8b8c95e603ce3
enesbayri <117692313+enesbayri@users.noreply.github.com>
2023-06-07 00:38:56 +0300

    güvenlik güncellemesi yapıldı

--- app/Http/Controllers/AdminReturnOrderController.php
+++ app/Http/Controllers/AdminReturnOrderController.php
@@ -10,6 +10,10 @@ use function GuzzleHttp\Promise\all;
 
 class AdminReturnOrderController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth', 'role:admin']);
+    }
     public function index()
     {
         $returnOrders=ReturnOrder::all();
